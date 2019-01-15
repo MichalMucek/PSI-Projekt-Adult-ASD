d73bcefe9efefcd4d7a1dad0bad76237f411772c
Michał Mucek <michal.mucek@gmail.com>
2019-01-15 21:32:51 +0100

    Ostatnie spotkanie

--- loaded_model.py
+++ loaded_model.py
@@ -1,9 +1,8 @@
 from keras.models import model_from_json
 import numpy as np
 
-dataset = np.loadtxt('csv_result-Autism-Adult-Data-MODIFIED.csv', delimiter=",")
+dataset = np.loadtxt('predict.csv', delimiter=",")
 
-Y = dataset[:, 19]
 X = dataset[:, 0:19]
 
 json_file = open('model.json', 'r')
@@ -15,8 +14,8 @@ loaded_model.load_weights("model.h5")
 
 loaded_model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-scores = loaded_model.evaluate(X, Y, verbose=0)
-print("%s: %.2f%%" % (loaded_model.metrics_names[1], scores[1] * 100))
+# scores = loaded_model.evaluate(X, Y, verbose=0)
+# print("%s: %.2f%%" % (loaded_model.metrics_names[1], scores[1] * 100))
 
 scores = loaded_model.predict_classes(X, verbose=0)
 print(scores)
